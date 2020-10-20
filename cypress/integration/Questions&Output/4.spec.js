d88ce241e1ee9e17ea9700291a82637c1499d061
pemawangyel07@gmail.com <pemawangyel07@gmail.com>
2020-10-20 07:13:11 +0600

    Sign in programatically

---
+++ cypress/integration/Questions&Output/4.spec.js
@@ -0,0 +1,8 @@
+const { createYield } = require("typescript")
+
+//sign in programatically
+describe('Login', ()=>{
+    it('login in programatically', ()=>{
+        cy.ProgramaticSingnIn()
+    })
+})
