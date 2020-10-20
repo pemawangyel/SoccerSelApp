6bde9709c889f8aced3d37c2822eefa0bf654369
pemawangyel07@gmail.com <pemawangyel07@gmail.com>
2020-10-20 08:25:31 +0600

    sorting of files and folders

---
+++ cypress/integration/TaskSelise/GeneralTesting/loginfromfixture.spec.js
@@ -0,0 +1,12 @@
+const { createYield } = require("typescript")
+
+describe('login', ()=>{
+    it('login from fixture data',()=>{
+        cy.visit('http://localhost:4200/user/login')
+        cy.fixture('login.json').then((user=>{
+            cy.get('input[placeholder="Email"]').type(user.email)
+            cy.get('input[placeholder="Password"]').type(user.pwd)
+            cy.get('.btn').contains('login').click()
+        }))
+    })
+})
